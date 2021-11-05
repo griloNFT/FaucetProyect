009e9249796b1025db5d4a7337c752e3ae1875f4
PatoVerde <leandrodenos@gmail.com>
2021-11-05 00:06:35 -0300

    CONTRATO STAKING SUCIO, demas ok

---
+++ contracts/token/PatoVerde.sol
@@ -0,0 +1,45 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.4.22 <0.9.0;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+//import "../contracts/TransactionFee.sol";
+
+contract PatoVerde is ERC20 {
+
+  
+  address public owner;
+  uint public maxSupply = 21000000 ether;
+  uint public supply = 0;
+  
+
+  /**
+   * @dev Grants `DEFAULT_ADMIN_ROLE`, `MINTER_ROLE` and `PAUSER_ROLE` to the
+   * account that deploys the contract and mint 1million tokens to master chef address
+   *
+   * See {ERC20-constructor}.
+   */
+  constructor(address masterChef) ERC20('Pato Verde', 'PVP') {
+    uint initial = 10000000 ether;
+    _mint(masterChef, initial);    
+    owner = msg.sender;    
+  }
+
+  /**
+   * @dev Creates `amount` new tokens for `to`.
+   *
+   * See {ERC20-_mint}.
+   *
+   * Requirements:
+   *
+   * - the caller must have the `MINTER_ROLE`.
+   */
+  function mint(address to, uint256 amount) public virtual {    
+    require(msg.sender == owner, "Mint: onlyOWner");
+    require((supply + amount) <= maxSupply, "Max supply alcanzado");
+    supply = supply + amount;
+    _mint(to, amount);
+  }
+  
+
+}
---
+++ migrations/2_contracts_migration.js
@@ -0,0 +1,64 @@
+const PatoVerde = artifacts.require('PatoVerde');
+const Staking = artifacts.require('Staking');
+const Reward = artifacts.require('Reward');
+const Faucet = artifacts.require("Faucet");
+const RandomToken = artifacts.require('RandomToken');
+
+module.exports = async function (deployer, network, accounts) {
+  
+  //const viellasPerBlock = web3.utils.toWei('0.1', 'ether');
+  const viellasPerBlock = web3.utils.toWei('1', 'ether');
+  var PATO;
+  var stk;
+  var rwrd;
+  //var rnd;
+  var fau;
+
+  if(network === 'development'){
+    await deployer.deploy(PatoVerde, accounts[0]);
+    PATO = await PatoVerde.deployed();
+
+    await deployer.deploy(Reward, PATO.address);
+    rwrd = await Reward.deployed();
+
+    await deployer.deploy(Staking, PATO.address, PATO.address, accounts[0] , viellasPerBlock, 0, rwrd.address);
+    await deployer.deploy(Faucet, accounts[0], PATO.address);
+
+  }else{
+    await deployer.deploy(PatoVerde, accounts[0]);
+    PATO = await PatoVerde.deployed();
+
+    await deployer.deploy(Reward, PATO.address);
+    rwrd = await Reward.deployed();
+
+    await deployer.deploy(Staking, PATO.address, PATO.address, accounts[0], viellasPerBlock, 0, rwrd.address);
+    await deployer.deploy(Faucet, accounts[0], PATO.address);
+  }
+/*
+  await deployer.deploy(RandomToken, web3.utils.toWei('1000000', 'ether'));
+
+  //rnd = await RandomToken.deployed(); 
+*/
+  fau = await Faucet.deployed()
+  stk = await Staking.deployed();
+
+  await rwrd.setStakingAddress(stk.address);
+  await PATO.setStakingMinter(stk.address);
+  
+
+
+
+  var content = "export const " + network + "TuViellaAddress = \"" + PATO.address + "\"\n";
+  content += "export const " + network + "FaucetAddress = \"" + fau.address + "\"\n";
+  content += "export const " + network + "StakingAddress = \"" + stk.address + "\"\n";
+  //content += "export const " + network + "RandomTokenAddress = \"" + rnd.address + "\"\n";
+
+  var fs = require('fs');
+  fs.writeFile(network + ".js", content, (err)=>{
+    if(err){
+      console.log(err);
+    }
+  });
+
+
+  };
