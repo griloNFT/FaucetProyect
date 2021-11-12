99904f8a4e7fbc438f341d57d3be4062b3783b02
guillermo <guille1000142@gmail.com>
2021-11-12 20:47:34 +0100

    home

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -1,24 +1,80 @@
 import React, { Component } from 'react'
 import ReactPlayer from "react-player"
+import Tokenomics from '../images/tokenomics.png'
+import Portada from '../images/token.png'
+
 
 class Home extends Component {
 
 
   render() {
     return (
+      <div>
       <article>
-        <h1 class="titles">WELCOME TO THE PVP COMMUNITY!</h1>
-        {/* <div>
-          <div id="twitch">
+        <h1 class="titles">PVP COMMUNITY</h1>
+        <div id="presentation">
+        <p><img src={Portada} /></p>
+            <div>
+            <h3>WHAT IS PVP?</h3>
+            <p>
+              Pato Verde Projects (PVP) is a community that it is focused on people who wants to study development of decentralized applications and smart contracts on the blockchain
+              and wants to launch his project together with members of the community or with his own work team
+            </p>
+            <p>
+              PHASE 1: Free token distribution and yield farming.
+            </p>
+            <p>
+              PHASE 2: Create or vote project ideas and add token value.
+            </p>
+            <p>
+              PHASE 3: Manage NFTs with API Twitch and game launch.
+            </p>
+            </div>
+            <div>
+            <h3>WHAT CAN YOU DO WITH PVP APP?</h3>
+            <p>
+              1. Claim free PVP tokens and NFTs on the app every 24 hours!
+              (Free claims will be available until 12/31/2021 23:59 UTC)
+            </p>
+            <p>   
+              2. Deposit PVP tokens in the pool to be rewarded with more PVP tokens!
+            </p>
+            <p>  
+              3. Create or vote project ideas on blockchain with PVP tokens!
+            </p>
+            <p>  
+              4. Manage NFT Marketplace PVP tokens!
+            </p>
+            </div>
+            <div>
+            <h3>ARE YOU A INFLUENCER?</h3>
+            <p>If you are a small or big influencer you can request for the creation of your project or token on the blockchain free!</p>
+            <p>In the future we will implement Api Twitch for important streamers that want to interact with followers using blockchain web3. </p>
+            </div>
+            <div>
+            <h3>TOKENOMICS</h3>
+            <img src={Tokenomics} width="250" height="250" alt="" />
+            <p>
+              INITIAL SUPPLY: 10.000.000 PVP
+            </p>
+            <p>
+              TOTAL SUPPLY: 21.000.000 PVP 
+            </p>
+          </div>
+        </div> 
+      </article>
+          <div class="boxModal" id="twitch">
+          <h3>STREAM</h3>
+          <div>
             <ReactPlayer
               width="640"
               height="360"
               url="https://www.twitch.tv/patoverde_pv"
               controls
             />
-          </div>  
-        </div> */}
-      </article>
+          </div> 
+        </div>
+        </div>
     );
   }
 }
