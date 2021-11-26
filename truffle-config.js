424033161492f0c4bcb1d46e1fd9e08f2f7b3cf3
guillermo <guille1000142@gmail.com>
2021-11-26 19:05:17 +0100

    nuevo truffle config

--- truffle-config.js
+++ truffle-config.js
@@ -11,35 +11,39 @@ module.exports = {
           process.env.MNENOMIC,
           "https://ropsten.infura.io/v3/" + process.env.INFURA_API_KEY
         ),
-
-      network_id: 3, // Ropsten's id
-      gas: 5500000, // Ropsten has a lower block limit than mainnet
-      confirmations: 2, // # of confs to wait between deployments. (default: 0)
-      timeoutBlocks: 200, // # of blocks before a deployment times out  (minimum/default: 50)
-      skipDryRun: true, // Skip dry run before migrations? (default: false for public nets )
+      network_id: 3,
+      gas: 5500000, 
+      confirmations: 1,
+      timeoutBlocks: 200,
+      skipDryRun: true, 
     },
 
-    development: {
-      host: '127.0.0.1',     // Localhost (default: none)
-      port: 7545,            // Standard Ethereum port (default: none)
-      network_id: '*'       // Any network (default: none)
+    bscMainnet: { 
+      provider: () =>
+        new HDWalletProvider(
+          process.env.MNENOMIC,
+          "https://bsc-dataseed.binance.org/"
+        ),
+      network_id: 56, 
+      confirmations: 1,
+      timeoutBlocks: 900, 
+      networkCheckTimeout: 999999,
+      skipDryRun: true, 
     },
-    
 
     bscTestnet: {
-      networkCheckTimeout: 10000,
       provider: () =>
         new HDWalletProvider(
           process.env.MNENOMIC,
           "https://data-seed-prebsc-1-s1.binance.org:8545/"
         ),
-
-      network_id: 97, // Ropsten's id
-      confirmations: 1, // # of confs to wait between deployments. (default: 0)
-      timeoutBlocks: 200, // # of blocks before a deployment times out  (minimum/default: 50)
-      //skipDryRun: true      
+      network_id: 97, 
+      confirmations: 1,
+      timeoutBlocks: 1000, 
+      networkCheckTimeout: 999999,
+      skipDryRun: true, 
     },
-    
+
   },
 
   plugins: [
@@ -50,17 +54,11 @@ module.exports = {
     bscscan: process.env.BSC_SCAN_API_KEY
  },
 
-  // Set default mocha options here, use special reporters etc.
-  mocha: {
-    // timeout: 100000
-  },
-
-  // Configure your compilers
   compilers: {
     solc: {
-      version: "0.8.0", // Fetch exact version from solc-bin (default: truffle's version)
-      // docker: true,        // Use "0.5.1" you've installed locally with docker (default: false)
-      settings: {          // See the solidity docs for advice about optimization and evmVersion
+      version: "0.8.0", 
+      // docker: true,       
+      settings: {        
         optimizer: {
           enabled: true,
           runs: 200
